764cf231da99fb98ab872780e0707593438735ee
azim <azimbabu3234@gmail.com>
2026-09-04 10:24:48 +0600

    iam making my first project

---
+++ WEB-DEVELOPEMENT/PROJECT/01..09.03.26..Background-Color-Changer/script.js
@@ -0,0 +1,6 @@
+let buttonOne = document.querySelector(".buttonOne");
+// buttonOne.addEventListener('click',() =>{
+//   console.log('iam button one')
+// })
+let buttonTwo = document.querySelector('.buttonTwo');
+let mainValue ='0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F';
